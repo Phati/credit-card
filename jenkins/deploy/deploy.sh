11f95a8fdcd36a7adc11e821f89663abd50eea48
Phati <phatisavant@gmail.com>
2024-07-09 11:43:21 +0530

    initial commit

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -12,5 +12,5 @@ docker pull $DOCKER_HUB_USER/$IMAGE:$RELEASE_VERSION
 
 echo "*** Checking Kubectl access ***"
 kubectl get ns
-helm upgrade --debug --dry-run -f $WORKSPACE/helm-charts/app/values/lms.yaml --set image.repository=$DOCKER_HUB_USER/$IMAGE --set image.tag=$RELEASE_VERSION --install lms $WORKSPACE/helm-charts/app --namespace cs --create-namespace
+helm upgrade --debug --dry-run -f $WORKSPACE/helm-charts/app/values/credit-card.yaml --set image.repository=$DOCKER_HUB_USER/$IMAGE --set image.tag=$RELEASE_VERSION --install creditcard $WORKSPACE/helm-charts/app --namespace cs --create-namespace
 helm upgrade -f $WORKSPACE/helm-charts/app/values/credit-card.yaml --set image.repository=$DOCKER_HUB_USER/$IMAGE --set image.tag=$RELEASE_VERSION --install creditcard $WORKSPACE/helm-charts/app --namespace cs --create-namespace
