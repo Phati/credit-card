aeef5ebfa73e6ec8faefeb6a7f2ea15bf7b0d599
Phati <phatisavant@gmail.com>
2024-07-09 11:26:55 +0530

    initial commit

---
+++ jenkins/build/mvn.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "*******************************"
+echo "***** Building Jar ************"
+echo "*******************************"
+
+echo "jar name --  $TARGET_JAR_NAME"
+echo "jar version --  $RELEASE_VERSION"
+
+
+docker run --rm  -v $WORKSPACE:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3.8.3-openjdk-17 "$@"
